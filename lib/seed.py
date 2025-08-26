a8da8d555c8e4ab936901f1c445b9467775a3ec8
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2025-08-26 11:23:57 +0300

    first commit

--- lib/seed.py
+++ lib/seed.py
@@ -1,16 +1,40 @@
-#!/usr/bin/env python3
-
 from faker import Faker
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from models import Game
+from models import Game, Base
 
 fake = Faker()
 
 if __name__ == '__main__':
-    
+   
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    
+    Base.metadata.create_all(engine)
+
+   
+    session.query(Game).delete()
+    session.commit()
+
+    print("Seeding 50 random games...")
+
+    
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for _ in range(50)
+    ]
+
+    
+    session.bulk_save_objects(games)
+    session.commit()
+
+    print("Seeding complete!")
+
